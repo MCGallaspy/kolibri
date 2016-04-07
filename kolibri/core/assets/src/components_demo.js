f9f8a92d1c6ea0ceefb441c7419ea95a6af6f5d0
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-04-06 19:07:00 -0700

    Stub out TextInputWithTagDisplay view
    But there's a bug... Marionette.Deferred is undefined.

--- kolibri/core/assets/src/components_demo.js
+++ kolibri/core/assets/src/components_demo.js
@@ -2,10 +2,18 @@
 var components = require('components');
 var logging = require('loglevel');
 var Plugin = require('plugin_base');
+var $ = require('jquery');
+var Backbone = require('backbone');
+Backbone.$ = $;
+var Mn = require('backbone.marionette');
+var _ = require('lodash');
 
 logging.setDefaultLevel(2);
 
 logging.info('Component demo loaded!');
+
+
+var app = new Mn.Application();
 
 
 var ComponentDemoPlugin = Plugin.extend({
@@ -16,9 +24,31 @@ var ComponentDemoPlugin = Plugin.extend({
 
     initialize: function() {
         logging.info('Demo initialized!');
-    },
+        app.start();
+    }
 });
 
+
+var TextInputWithTagDisplay = Mn.LayoutView.extend({
+    template: function(serialized_model) {
+        return _.template(this.template_html)({});
+    },
+
+    template_html: '<div>Foo!</div>'
+});
+
+
+app.on('start', function(){
+    app.addRegions({
+        content: '#content'
+    });
+
+    var tiwtd = new TextInputWithTagDisplay();
+    app.getRegion('content').show(tiwtd);
+});
+
+
 var cpd = new ComponentDemoPlugin();
+
 
 module.exports = {};
